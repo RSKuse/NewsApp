b57c071bfa6f570fb49ce7fbbb1ddc84c4040c43
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2023-07-12 22:57:18 +0200

    Initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Reuben Simphiwe Kuse on 2023/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
