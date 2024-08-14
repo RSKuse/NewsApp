e4287734926c20af892fea4e80aa210c6d5c92c9
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-08-15 00:44:21 +0200

    contraints

--- NewsApp/NewsViewControllers/Cell/CountryTableViewCell.swift
+++ NewsApp/NewsViewControllers/Cell/CountryTableViewCell.swift
@@ -9,21 +9,21 @@ import Foundation
 import UIKit
 
 class CountryTableViewCell: UITableViewCell {
-
+    
     lazy var flagImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-
+    
     lazy var countryLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     lazy var checkmarkImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "checkmark")
@@ -32,35 +32,35 @@ class CountryTableViewCell: UITableViewCell {
         imageView.isHidden = true
         return imageView
     }()
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     func setupUI() {
         addSubview(flagImageView)
         addSubview(countryLabel)
         addSubview(checkmarkImageView)
-
-        NSLayoutConstraint.activate([
-            flagImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            flagImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            flagImageView.widthAnchor.constraint(equalToConstant: 30),
-            flagImageView.heightAnchor.constraint(equalToConstant: 20),
-
-            countryLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-            countryLabel.leadingAnchor.constraint(equalTo: flagImageView.trailingAnchor, constant: 20),
-
-            checkmarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            checkmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
-        ])
+        
+        
+        flagImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        flagImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        flagImageView.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        flagImageView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        countryLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        countryLabel.leadingAnchor.constraint(equalTo: flagImageView.trailingAnchor, constant: 20).isActive = true
+        
+        checkmarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        checkmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+        
     }
-
+    
     func configure(with country: NewsCountry, isSelected: Bool) {
         countryLabel.text = country.rawValue.uppercased()
         flagImageView.image = UIImage(named: "flag_\(country.rawValue)") // This line sets the flag image
--- NewsApp/NewsViewControllers/Controllers/SettingsViewController.swift
+++ NewsApp/NewsViewControllers/Controllers/SettingsViewController.swift
@@ -15,10 +15,10 @@ class SettingsViewController: UIViewController {
     
     weak var delegate: SettingsViewControllerDelegate?
     
-
+    
     let viewModel = NewsViewModel()
     var selectedCountry: NewsCountry?
-
+    
     lazy var countriesTableView: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
@@ -27,25 +27,25 @@ class SettingsViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
-
+    
     func setupUI() {
         view.backgroundColor = .white
         title = "Settings"
         navigationItem.largeTitleDisplayMode = .never
         selectedCountry = viewModel.selectedCountry
         view.addSubview(countriesTableView)
-
-        NSLayoutConstraint.activate([
-            countriesTableView.topAnchor.constraint(equalTo: view.topAnchor),
-            countriesTableView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            countriesTableView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            countriesTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
+        
+        
+        countriesTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        countriesTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        countriesTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        countriesTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
     }
 }
 
